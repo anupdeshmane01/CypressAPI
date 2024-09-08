f40239e13fcbe0c4159629930fd2f05b81dd87db
anupdeshmane01 <anupdeshmane01@gmail.com>
2024-09-09 00:13:09 +0530

    103

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  reporter: "cypress-mochawesome-reporter",
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    charts: true,
-    reportPageTitle: 'custom-title',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
+    reportDir: 'cypress/reports/.jsons',
+    overwrite: false,
+    html: false,
+    json: true
   },
-
   e2e: {
-    retries: 1,
-    failOnStatusCode: false,
     setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
