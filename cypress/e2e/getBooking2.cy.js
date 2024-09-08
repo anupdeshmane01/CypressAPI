488dbaef4aae46065fa8eaadab848c020d39613a
unknown <anupdeshmane01@gmail.com>
2024-09-08 10:40:25 +0530

    First Commit3

---
+++ cypress/e2e/getBooking2.cy.js
@@ -0,0 +1,27 @@
+describe('Get booking ID',()=>{
+
+    it('Get Booking ID',()=>{
+
+        cy.request({
+
+            method: "GET",
+            url:"https://restful-booker.herokuapp.com/booking/785",
+            headers: {
+                Authorization: "Bearer dc39e491e666daf"
+                
+            },
+            failOnStatusCode: false
+        })
+        .then((response)=>{
+            cy.log(JSON.stringify(response))
+            expect(response.status).to.equal(200)
+        })
+
+
+
+    })
+
+
+
+
+})
